3f518e5e156d9089721ca6e2705b96a4b46c4ef8
Ryan Crispin Heneise <ryan@mysmallidea.com>
2017-10-26 11:09:58 -0500

    Extracted the "Resolve Issue" button into a helper method.

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,18 @@
 module IssuesHelper
+  def resolve_issue_button(issue)
+    link_to "#{issue.resolved? ? 'Reopen' : 'Resolve'} Issue", resolve_issue_path,
+            method: :put, 
+            remote: true, 
+            class: resolve_issue_button_class(issue), 
+            id: 'resolve_button'
+  end
+
+  private
+  def resolve_issue_button_class(issue)
+    if issue.resolved?
+      'btn btn-danger'  # Red button
+    else
+      'btn btn-warning' # Orange button
+    end
+  end
 end
