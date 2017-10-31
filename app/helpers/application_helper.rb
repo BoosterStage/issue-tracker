ca370008b23a03c20604111abb09004dcc494e16
Ryan Crispin Heneise <ryan@mysmallidea.com>
2017-10-31 15:04:45 -0500

    Added flash notice helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def flash_notice(flash) 
+    capture do
+      flash.each do |type, message|
+        concat content_tag :div, message, class: "alert alert-#{flash_class(type)}", role: "alert"
+      end
+    end
+  end
+  
+  def flash_class(type)
+    {'notice' => 'success',
+     'error' => 'danger',
+     'alert' => 'info'
+    }[type]
+  end
+
 end
